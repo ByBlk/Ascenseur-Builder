3e26f337d2ff8517787daed75fa2eca09426bc8b
BŁ₭ <atlantafa0@gmail.com>
2025-01-06 18:01:02 +0100

    Create Config.lua

---
+++ shared/Config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Marker = 1 -- Numéro du marqueur que tu veux mettre marker https://docs.fivem.net/docs/game-references/markers/
+Config.DistanceVu = 5.0 
+Config.DistanceAction = 2.0
+Config.Perms = {"fondateur"}
+Config.Couleurpoint = {255, 0, 150} -- Couleur des points https://docs.fivem.net/docs/game-references/hud-colors/
